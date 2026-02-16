d1246fabedf9e8452fb4bfb941210cf3a28cbc0f
eee1eli <166856755+eee1eli@users.noreply.github.com>
2026-02-16 09:19:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using lab2;
+
+class Program
+{
+    static void Main()
+    {
+        string text = "Hello World";
+        Console.WriteLine(MyFunctions.ReverseString(text));
+
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        MyFunctions.PrintArray(numbers);
+    }
+}
